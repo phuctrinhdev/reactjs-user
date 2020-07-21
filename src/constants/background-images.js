2900e816c77b89d457e2bb720be5ea768a76d24e
Hoang Phuc <>
2020-07-21 22:12:49 +0700

    Axios client setup

---
+++ src/constants/background-images.js
@@ -0,0 +1,15 @@
+import blueBackground from '../assets/images/blue-bg.jpg';
+import colorfulBackground from '../assets/images/colorful-bg.jpg';
+import orangeBackground from '../assets/images/orange-bg.jpg';
+import pinkBackground from '../assets/images/pink-bg.jpg';
+import whiteBackground from '../assets/images/blue-bg.jpg';
+
+const BackgroundImages = {
+  BLUE_BG: blueBackground,
+  COLORFUL_BG: colorfulBackground,
+  ORANGE_BG: orangeBackground,
+  PINK_BG: pinkBackground,
+  WHITE_BG: whiteBackground,
+}
+
+export default BackgroundImages;
